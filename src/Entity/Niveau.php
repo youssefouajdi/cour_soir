324fa340e618501e6673e152e703bfd425133639
youssefouajdi <youssefouajdi1@gmail.com>
2020-10-11 10:27:49 +0000

    ajout de la table  niveau 10/11/2020

---
+++ src/Entity/Niveau.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NiveauRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NiveauRepository::class)
+ */
+class Niveau
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_niveau;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNomNiveau(): ?string
+    {
+        return $this->nom_niveau;
+    }
+
+    public function setNomNiveau(string $nom_niveau): self
+    {
+        $this->nom_niveau = $nom_niveau;
+
+        return $this;
+    }
+}
